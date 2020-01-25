9eba1b77fd635c2490a742feb3868af60fd6627b
R1mzy2007 <58106395+R1mzy2007@users.noreply.github.com>
2020-01-25 12:29:24 +0200

    New Update

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
-
+session_start();
 require 'vendor/autoload.php';
-
+$n = 228;
 $app = new \atk4\ui\App('Roma');
 $app->initLayout('Centered');
 
@@ -13,7 +13,6 @@ $col_2 = $columns->addColumn(10);
 $col_3 = $columns->addColumn(3);
 
 $clicker = $col_2->add(["Button","228","yellow fluid big"]);
-$clicker->on('click', function($clicker) {
-     $clicker->set("Test");
-    $clicker->js()->reload();
-  });
+$label = $col_2->add(["Label",$_SESSION['user_id']]);
+
+$save = $col_2->add(["Button","Save","blue big"]);
